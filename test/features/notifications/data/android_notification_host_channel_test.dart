7e96bab007335083b8b17a5cdf7386e8c0804157
Roman Kovach <idcfyoy@gmail.com>
2026-04-29 00:39:20 +0300

    feat: FINALLY PUSH NOTIFICATIONS WORK

--- test/features/notifications/data/android_notification_host_channel_test.dart
+++ test/features/notifications/data/android_notification_host_channel_test.dart
@@ -33,4 +33,41 @@ void main() {
     );
     expect(source, contains('android:value="sgtp_app_notifications"'));
   });
+
+  test('Android manifest declares FCM default notification appearance', () {
+    final source = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync().replaceAll('\r\n', '\n');
+
+    expect(
+      source,
+      contains(
+        'android:name="com.google.firebase.messaging.default_notification_icon"',
+      ),
+    );
+    expect(
+      source,
+      contains('android:resource="@drawable/ic_stat_sgtp_notification"'),
+    );
+    expect(
+      source,
+      contains(
+        'android:name="com.google.firebase.messaging.default_notification_color"',
+      ),
+    );
+    expect(
+      source,
+      contains('android:resource="@color/sgtp_notification_color"'),
+    );
+    expect(
+      File(
+        'android/app/src/main/res/drawable/ic_stat_sgtp_notification.xml',
+      ).existsSync(),
+      isTrue,
+    );
+    expect(
+      File('android/app/src/main/res/values/colors.xml').existsSync(),
+      isTrue,
+    );
+  });
 }
